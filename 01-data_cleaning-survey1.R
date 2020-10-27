6b2f39e01ec7f2fa5d58cac580a0aa1ce5d2acd4
Hyo-Eun Park <61765475+hynprk@users.noreply.github.com>
2020-10-27 16:13:57 +0900

    Add files via upload

---
+++ 01-data_cleaning-survey1.R
@@ -0,0 +1,112 @@
+#### Preamble ####
+# Purpose: Prepare and clean the survey data downloaded from Voter Study Group
+# - Link to Voter Study Group: https://www.voterstudygroup.org/publication/nationscape-data-set
+# Author: Pamela De Vera, Hyoeun Park
+# Data: 02 November 2020
+# Contact: pamela.devera@mail.utoronto.ca, hyoeun.park@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the data 
+# from https://www.voterstudygroup.org/publication/nationscape-data-set 
+# and save the folder that you're interested in to election_data/survey 
+# - Don't forget to gitignore it!
+
+
+#### Workspace setup ####
+library(haven)
+library(tidyverse)
+# setwd("~/Desktop/STA304/Problem Set 3")
+# Read in the raw data
+raw_data <- read_dta("election_data/survey/ns20200625.dta")
+# Add the labels
+raw_data <- labelled::to_factor(raw_data)
+
+### Selecting specific variables only ###
+cleaned_data <- 
+  raw_data %>% 
+  select(vote_2020,
+         gender,
+         census_region,
+         hispanic,
+         race_ethnicity,
+         household_income,
+         education,
+         age)
+
+### More Data Cleaning ### 
+# Remove any Missing entries (i.e., NA's)
+cleaned_data <- na.omit(cleaned_data)
+
+# Change variable name from 'gender' to 'sex'
+## - so that variable name is the same as census data
+names(cleaned_data)[names(cleaned_data) == 'gender'] <- 'sex'
+# Create new variable: is_male
+cleaned_data <- cleaned_data %>% 
+  mutate(is_male = 
+           ifelse(sex == "Male", 1, 0))
+
+# Binary Outcome for 'vote_2020'
+## filter Donald Trump and Joe Biden only
+cleaned_data <- cleaned_data %>%
+  filter(vote_2020 == "Donald Trump" | vote_2020 == "Joe Biden")
+## Create new variable
+cleaned_data<-
+  cleaned_data %>%
+  mutate(vote_biden = 
+           ifelse(vote_2020=="Joe Biden", 1, 0))
+
+# Binary outcome for Hispanic
+cleaned_data <- cleaned_data %>%
+  mutate(is_hispanic = 
+           ifelse(hispanic == "Not Hispanic", 0, 1))
+
+# Stratifying: Race Ethnicity into 5 categories
+cleaned_data <- cleaned_data %>% 
+  mutate(race_ethnicity_division =
+           ifelse(race_ethnicity == "White", 
+                  "White", 
+                  ifelse(race_ethnicity == "Black, or African American", 
+                         "Black", 
+                         ifelse(race_ethnicity == "American Indian or Alaska Native", 
+                                "Indigenous", 
+                                ifelse(race_ethnicity == "Some other race", 
+                                       "Other", 
+                                       "Asian or Pacific Islander")))))
+
+# Stratifying: Education Levels
+cleaned_data <- cleaned_data %>%
+  mutate(education_level = 
+           ifelse(education == "3rd Grade or less" | 
+                    education == "Middle School - Grade 4 - 8" | 
+                    education == "Completed some high school" | 
+                    education == "High school graduate" | 
+                    education == "Completed some college, but no degree", 
+                  "Primary/Secondary Education", 
+                  ifelse(education == "Other post high school vocational training" | 
+                           education == "Associate Degree" | 
+                           education == "College Degree (such as B.A., B.S.)" | 
+                           education == "Completed some graduate, but no degree", 
+                         "Post Secondary training/degree", 
+                         "Master's/PhD Degree")))
+
+# Stratifying: Income Class
+cleaned_data <- cleaned_data %>% 
+  mutate(income_class = 
+           ifelse(household_income == "$125,000 to $149,999" |
+                    household_income == "$150,000 to $174,999" | 
+                    household_income == "$175,000 to $199,999" |
+                    household_income == "$200,000 to $249,999" |
+                    household_income == "$250,000 and above", 
+                  "High", 
+                  ifelse(household_income == "Less than $14,999" |
+                           household_income == "$15,000 to $19,999" | 
+                           household_income == "$20,000 to $24,999" |
+                           household_income == "$25,000 to $29,999" |
+                           household_income == "$30,000 to $34,999" |
+                           household_income == "$35,000 to $39,999" |
+                           household_income == "$40,000 to $44,999", "Low", 
+                         "Middle")))
+
+# Saving the survey/sample data as a csv file in my
+# working directory
+write_csv(cleaned_data, "election_data/output/survey_data.csv")
